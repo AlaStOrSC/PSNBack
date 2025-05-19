2ca44421916a56346263b4010aea7c53ece81692
EnriqueMacias <rain0fwolfs@gmail.com>
2025-05-19 19:46:50 +0200

    prueba en token posible error de servidor con el expire date

--- src/services/userService.js
+++ src/services/userService.js
@@ -46,7 +46,7 @@ const login = async ({ email, password }) => {
     throw new Error('Correo electrónico o contraseña incorrectos');
   }
 
-  const token = jwt.sign({ userId: user._id, role: user.role }, jwtSecret, { expiresIn: '1h' });
+  const token = jwt.sign({ userId: user._id, role: user.role }, jwtSecret, { expiresIn: '100d' });
 
   return {
     user: {

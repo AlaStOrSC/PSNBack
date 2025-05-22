2f68b6ff01c6f0aa11b5fb723dc4b8a7bb05789b
EnriqueMacias <rain0fwolfs@gmail.com>
2025-05-22 15:15:01 +0200

    ruta del front

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'https://padel-social-frontend.onrender.com',
   'http://localhost:5173',
-  'https://psn-frontend.vercel.app/'
+  'https://psn-frontend.vercel.app'
 ];
 
 app.use(cors({

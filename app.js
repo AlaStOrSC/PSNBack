46ce33509a614874c6aee27be6ece54d13291a53
EnriqueMacias <rain0fwolfs@gmail.com>
2025-06-02 13:34:11 +0200

    fixing rutas

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const notFoundHandler = require('./src/middlewares/notFoundHandler');
 const adminRoutes = require('./src/routes/adminRoutes');
 const productRoutes = require('./src/routes/productRoutes');
 const cron = require('node-cron');
-const { deleteExpiredMatchesWithEmptySlots } = require('./services/matchService');
+const { deleteExpiredMatchesWithEmptySlots } = require('./src/services/matchService');
 
 const app = express();
 
